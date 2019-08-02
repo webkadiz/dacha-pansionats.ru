b43d89afe97e0df3b3f27944b06281a5686b3ad8
Tkachenko Vladislav <webkadiz@gmail.com>
2019-08-03 00:11:57 +0300

    big update

--- src/index.js
+++ src/index.js
@@ -1,28 +1,112 @@
-import "bootstrap/dist/css/bootstrap-grid.css";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import "./scss/main.scss";
-
+import "./scss/security-fire.scss";
 import $ from "jquery";
 import "slick-carousel";
 
-$(".practice__slider").slick({
-  prevArrow: "<div class='prev-arrow'></div>",
-  nextArrow: "<div class='next-arrow'></div>",
-  asNavFor: ".practice__slider-miniature",
-  adaptiveHeight: true,
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: { arrows: false, dots: true, dotsClass: "practice__slider-dots" }
-    }
-  ]
-});
+setDates();
+resizeWindow();
+initSliders();
 
-$(".practice__slider-miniature").slick({
-  slidesToShow: 8,
-  asNavFor: ".practice__slider",
-  focusOnSelect: true
-});
+window.addEventListener("resize", resizeWindow);
 
-console.log(23);
+function initSliders() {
+  $(".practice__slider").slick({
+    asNavFor: ".practice__slider-miniature",
+    fade: true,
+    adaptiveHeight: true,
+    swipe: false,
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: { arrows: false, dots: true, dotsClass: "practice__slider-dots", swipe: true, fade: false }
+      }
+    ]
+  });
+
+  $(".practice__slider-miniature").slick({
+    prevArrow: "<div class='prev-arrow'></div>",
+    nextArrow: "<div class='next-arrow'></div>",
+    vertical: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    asNavFor: ".practice__slider",
+    focusOnSelect: true,
+    swipe: false,
+    responsive: [
+      {
+        breakpoint: 1100,
+        settings: { slidesToShow: 4 }
+      },
+      {
+        breakpoint: 861,
+        settings: { slidesToShow: 3 }
+      }
+    ]
+  });
+}
+
+function setDates() {
+  let setMonth = month => (month === 0 ? 12 : month);
+  let scheduleDescAll = $(".schedule__desc strong");
+
+  let sheduleService = scheduleDescAll.eq(0);
+  let sheduleInspection = scheduleDescAll.eq(1);
+  let sheduleBriefing = scheduleDescAll.eq(2);
+  let date = new Date();
+  let cYear = +date.getFullYear();
+  let cMonth = +date.getMonth() + 1;
+  let cDay = +date.getDate();
+  let sMonth, sYear, iDay, iMonth, iYear, bMonth, bYear;
+
+  if (cDay >= 10) {
+    sMonth = cMonth;
+    sYear = cYear;
+  } else {
+    sMonth = setMonth(cMonth - 1);
+    sYear = cMonth === 1 ? cYear - 1 : cYear;
+  }
+
+  if (cDay >= 17) {
+    bMonth = cMonth;
+    bYear = cYear;
+  } else {
+    bMonth = setMonth(cMonth - 1);
+    bYear = cMonth === 1 ? cYear - 1 : cYear;
+  }
+
+  if (cDay >= 12 && cDay < 26) {
+    iDay = 12;
+    iMonth = cMonth;
+    iYear = cYear;
+  } else if (cDay < 12 && cDay > 0) {
+    iDay = 26;
+    iMonth = setMonth(cMonth - 1);
+    iYear = cMonth === 1 ? cYear - 1 : cYear;
+  } else {
+    iDay = 26;
+    iMonth = cMonth;
+    iYear = cYear;
+  }
+
+  if (sMonth < 10) sMonth = "0" + sMonth;
+  if (iMonth < 10) iMonth = "0" + iMonth;
+  if (bMonth < 10) bMonth = "0" + bMonth;
+
+  sheduleService.text(`10.${sMonth}.${sYear}`);
+  sheduleInspection.text(`${iDay}.${iMonth}.${iYear}`);
+  sheduleBriefing.text(`17.${bMonth}.${bYear}`);
+}
+
+function resizeWindow() {
+  let tagline = $(".tagline");
+  let docWidth = $(window).width();
+
+  if (docWidth > 1200) {
+    docWidth = docWidth > 1400 ? 1400 : docWidth;
+
+    let sideWidth = (docWidth - 1200) / 2;
+    tagline.css({
+      marginLeft: -sideWidth,
+      marginRight: -sideWidth
+    });
+  }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,59 +1,86 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserJSPlugin = require("terser-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = {
-  entry: "./src/index.js",
+
+module.exports = env => ({
+  entry: { 
+    'security-fire': "./src/index.js",
+    slick: './src/slick.js'
+  },
   output: {
-    path: __dirname + "/dist",
-    filename: "build.js",
-    publicPath: "/dist/"
+    path: __dirname + "/build",
+    filename: env === "development" ? "[name].js" : "[name].production.js",
+    publicPath: "/"
+  },
+  optimization: {
+    minimizer: [new OptimizeCssAssetsPlugin(), new TerserJSPlugin()]
   },
   module: {
     rules: [
       {
-        test: /\.html$/,
-        use: ["html-loader"]
+        test: /\.js/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
       },
       {
-        test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"]
+        test: /\.scss/,
+        use: [
+          "style-loader",
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              data: "$dev: " + (env === 'development') + ";"
+            }
+          }
+        ]
       },
       {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        test: /\.css/,
+        use: ["style-loader", MiniCssExtractPlugin.loader, "css-loader"]
       },
       {
-        test: /\.(eot|ttf|woff|png|svg|gif)$/,
+        test: /\.(eot|ttf|woff|png|svg|gif|jpg)/,
         use: [
           {
             loader: "file-loader",
             options: {
-              name: "[name].[ext]",
-              outputPath: (name, path) => {
-                console.log(name);
-                if (/fonts/.test(path)) {
-                  return `fonts/${name}`;
-                } else if (/img/.test(path)) {
-                  return `img/${name}`;
-                } else if (/node_modules/.test(path)) {
-                  return `lib/${name}`;
-                } else {
-                  return `unknown/${name}`;
-                }
-              }
+              name: "security-fire-assets/[name].[ext]"
             }
-          }
+          },
+          "image-webpack-loader"
         ]
+      },
+      {
+        test: /\.html/,
+        use: {
+          loader: "html-loader",
+          options: {
+            attrs: [":data-src", ":src", ":style"]
+          }
+        }
       }
     ]
   },
   devServer: {
-    contentBase: "src/",
-    watchContentBase: true
+    port: 9000,
+    overlay: true
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
-      minify: true
-    })
+      template: "./src/index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: env === "development" ? "[name].css" : "[name].production.css",
+    }),
+    new CleanWebpackPlugin()
   ]
-};
+});
